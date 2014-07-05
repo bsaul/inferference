fefb3aa3ecd13df0248098dfbd6390220d24f8dc
bradley <bradley@bs.local>
2014-07-05 11:24:25 -0400

    revises V_matrix function to be more general

--- R/ipw_variance_estimates.R
+++ R/ipw_variance_estimates.R
@@ -30,21 +30,49 @@ Bscore <- function(predictors, B, G, theta, data){
 #' @export
 #' 
 #-----------------------------------------------------------------------------#
-V_matrix_effect <- function(Bscores, ipw_obj, alpha1, alpha2, trt.lvl1, trt.lvl2){
+V_matrix_effects <- function(Bscores, ipw_obj, alpha1, alpha2, 
+                             trt.lvl1, trt.lvl2, effect, marginal,
+                             na.rm = FALSE){
 
-  N <- ipw_obj$summary$ngroups
+  N <- dim(Bscores)[1]
   p <- dim(Bscores)[2]
+  a1 <- alpha1
+  a2 <- alpha2
+  t1 <- trt.lvl1
+  t2 <- trt.lvl2
+  
+  ## replace any Bscores with 0 ##
+  if(na.rm == FALSE) {
+    Bscores[is.na(Bscores)] <- 0
+  }
+  
+  fff <- ifelse(marginal == TRUE, 'marginal_outcomes', 'outcomes')
+  
+  if(effect == 'contrast'){   
+    x <- ipw_obj[[fff]]$contrasts$group_resid
+    if(marginal == TRUE){
+      x <- x[a1, a2, ]
+    } else {
+      x <- x[a1, t1, a2, t2, , a1, t1, a2, t2]
+    }
+  } 
+  else if(effect == 'outcome'){
+    x <- ipw_obj[[fff]]$group_resid
+    if(marginal == TRUE){
+      x <- x[ , a1]
+    } else {
+      x <- x[  ,a1, t1]
+    }
+  }
   
   tmp <- array(dim = c(p+1, p+1, N))
-
+  
   for(ii in 1:N){
-    hold <- c(Bscores[ii, ], ipw_obj$ipw$contrasts$group_resid[alpha1, trt.lvl1, alpha2, trt.lvl2, ii, alpha1, trt.lvl1, alpha2, trt.lvl2])
+    hold <- c(Bscores[ii, ], x[ii])
     tmp[ , , ii] <- hold %*% t(hold)
   }
   
-  V <- apply(tmp, 1:2, sum, na.rm = T)/N
+  V <- apply(tmp, 1:2, mean, na.rm = T)
 
   return(V)
 }
-
-
