e682181b2822c2620e40c2e014b2ccf2c3354878
bradley <bradley@bs.local>
2014-07-04 21:33:45 -0400

    removes Upartial() which is now incorporated into ipw_point_estimates() which is now equipped to handle arrays

--- R/ipw_point_estimates.R
+++ R/ipw_point_estimates.R
@@ -3,7 +3,7 @@
 #'  
 #' @param y vector of unweighted group means (i.e. output from \code{\link{group_means}}
 #' @param a treatment level (0,1) for which to compute weights. Defaults to NULL which returns marginal.
-#' @param weights weight matrix to use
+#' @param weights weight matrix/array to use
 #' @param rescale.factor factor by which to rescale values
 #' @param na.rm exclude groups missing weights? defaults to FALSE.
 #' @return length(alpha) vector of IPW estimates
@@ -11,95 +11,141 @@
 #-----------------------------------------------------------------------------#
 
 ipw_point_estimates <- function(y, G, A, B, data, weights,
-                                rescale.factor, na.rm = FALSE){
+                                 rescale.factor, na.rm = FALSE){
+  
+  ## DEFINE OBJECTS NEEDED FOR FUNCTION ##
   out <- list()
   
   groups <- as.numeric(dimnames(weights)[[1]])
-  alphas <- as.numeric(dimnames(weights)[[2]])
+  alphas <- as.numeric(dimnames(weights)[[length(dim(weights))]])
   trt_lvls <- sort(unique(data[, A]))
   
   N <- length(groups)
   k <- length(alphas)
   l <- length(trt_lvls)
+  p <- ifelse(is.matrix(weights), 1, dim(weights)[2])
   
-  # Summary information
+  ## Generalize function to work on arrays. Add a dimension to matrices. ##
+  if(is.matrix(weights)){
+    weights <- array(c(weights, 1), dim=c(N, 1, k))
+    predictors <- NULL
+  } else {
+    predictors <- dimnames(weights)[[2]]
+  }
+  
+  ## replace any missing weights with 0 ##
+  if(na.rm == FALSE) {
+    weights[is.na(weights)] <- 0
+  }
+  
+  ## SUMMARY ## 
   out$summary <- list(ngroups = N, 
                       nalphas = k,
                       ntreatments = l,
                       alphas = alphas,
                       treatments = trt_lvls)  
-
-  # replace any missing weights with 0
-  if(na.rm == FALSE) {
-    weights[is.na(weights)] <- 0
-  }
-    
+  
   ## CALCULATE MARGINAL ESTIMATES ####
   ybar <- group_means(Y = y, A = A, G = G, a = NA, data = data)
-  grp_est <- apply(weights, 2, function(x) x * ybar) * rescale.factor
-
-  out$marginal_outcomes$groups <- grp_est
-  out$marginal_outcomes$overall <- ymarg <- apply(grp_est, 2, mean, na.rm = TRUE)
+  
+  grp_est <- apply(weights, 2:3, function(x) x * ybar) * rescale.factor
+  dimnames(grp_est) <- list(groups, predictors, alphas)
+  
+  oa_est <- apply(grp_est, 2:3, mean, na.rm = TRUE)
+  #ymarg <- apply(grp_est, 2:3, mean, na.rm = TRUE)
+  
+  out$marginal_outcomes$groups <- drop(grp_est)
+  out$marginal_outcomes$overall <- drop(oa_est)
   
   ## CALCULATE OVERALL EFFECTS ####
-  out$ipw$marginal_outcomes$contrasts$overall <- outer(ymarg, ymarg, '-')
-  hold_oe_grp <- array(dim = c(k, k, N), dimnames = list(alphas, alphas, groups))
   
-  for(ii in 1:N){
-    hold_oe_grp[ , , ii] <- outer(grp_est[ii, ], grp_est[ii, ], '-')
+  hold_oe_overall <- array(dim = c(k, k, p),
+                           dimnames = list(alphas, alphas, predictors))
+  hold_oe_grp <- array(dim = c(k, k, N, p), 
+                       dimnames = list(alphas, alphas, groups, predictors))
+  
+  for(pp in 1:p){
+    hold_oe_overall[ , , pp] <- outer(oa_est[pp, ], oa_est[pp, ], '-')
+    
+    for(ii in 1:N){
+      hold_oe_grp[ , , ii, pp] <- outer(grp_est[ii, pp, ], grp_est[ii, pp, ], '-')
+    }
   }
   
-  out$marginal_outcomes$contrasts$overall <- hold_oe_grp
-
+  out$marginal_outcomes$contrasts$overall <- drop(hold_oe_overall)
+  out$marginal_outcomes$contrasts$groups <- drop(hold_oe_grp)
+  
   ## CALCULATE OUTCOME ESTIMATES PER TREATMENT LEVEL####
-
-  hold_grp <- hold_grp_diff <- array(dim = c(N, k, l),
-                    dimnames = list(groups, alphas, trt_lvls))
-  hold_oal <- array(dim = c(k, l),
-                    dimnames = list(alphas, trt_lvls))
+  
+  hold_grp <- hold_grp_diff <- array(dim = c(N, p, k, l),
+                                     dimnames = list(groups, predictors, 
+                                                     alphas, trt_lvls))
+  hold_oal <- array(dim = c(p, k, l),
+                    dimnames = list(predictors, alphas, trt_lvls))
   
   for(ll in 1:l){    
     a <- trt_lvls[ll]
-
+    
     # Compute means per group
     ybar_trt <- group_means(Y = y, A = A, G = G, a = a, data = data)
     
     # Modify weights per treatment level
-    weights_trt <- t(t(weights)/((alphas^a*(1-alphas)^(1-a))))
+    weights_trt <- array(dim= c(N, p, k),
+                         dimnames = list(1:N, predictors, alphas))
+    for(pp in 1:p){
+      weights_trt[ , pp, ] <- t(t(weights[ , pp, ])/((alphas^a*(1-alphas)^(1-a))))
+    }
+    
+    #weights_trt <- t(t(weights)/((alphas^a*(1-alphas)^(1-a))))
     
     # Compute estimates
-    grp_est <- apply(weights_trt, 2, function(x) x * ybar_trt) * rescale.factor
-    oal_est <- apply(grp_est, 2, mean, na.rm = TRUE)
+    grp_est <- apply(weights_trt, 2:3, function(x) x * ybar_trt) * rescale.factor
+    oal_est <- apply(grp_est, 2:3, mean, na.rm = TRUE)
     
-    hold_grp[ , , ll] <- grp_est
-    hold_oal[ , ll]   <- oal_est
+    hold_grp[ , , , ll] <- grp_est
+    hold_oal[ , , ll]   <- oal_est
     
     for(kk in 1:k){
       for(ii in 1:N){
-        hold_grp_diff[ii, kk, ll] <- hold_grp[ii, kk, ll] - hold_oal[kk, ll]
+        hold_grp_diff[ii, , kk, ll] <- hold_grp[ii,  , kk, ll] - hold_oal[ , kk, ll]
       }
     }
   }
-
-  out$outcomes <- list(groups = hold_grp, 
-                       overall = hold_oal, 
-                       group_resid = hold_grp_diff)
+  
+  out$outcomes <- list(groups = drop(hold_grp), 
+                       overall = drop(hold_oal), 
+                       group_resid = drop(hold_grp_diff))
   
   ## CALCULATE EFFECT CONTRASTS ####
-  hold_contrasts_oal <- outer(hold_oal, hold_oal, '-')
   
-  hold_contrast_grp <- array(dim = c(k, l, k, l, N),
-                             dimnames = list(alphas, trt_lvls,
-                                             alphas, trt_lvls, groups))
-
-  for(ii in 1:N){
-    hold_contrast_grp[ , , , , ii] <- outer(hold_grp[ii, , ], hold_grp[ii, , ], '-')
+  hold_contrasts_oal <- array(dim = c(k, l, k, l, p),
+                              dimnames = list(alphas, trt_lvls, alphas, 
+                                              trt_lvls, predictors))
+  
+  hold_contrast_grp <- array(dim = c(k, l, k, l, N, p),
+                             dimnames = list(alphas, trt_lvls, alphas, trt_lvls, 
+                                             groups, predictors))
+  
+  hold_resid <- array(dim = c(k, l, k, l, N, k, l, k, l, p),
+                      dimnames = list(alphas, trt_lvls, alphas, trt_lvls, 
+                                      groups, alphas, trt_lvls, alphas, trt_lvls, 
+                                      predictors))
+  
+  for(pp in 1:p){
+    hold_contrasts_oal[ , , , , pp] <- outer(hold_oal[pp, , ], hold_oal[pp, , ], '-')
+    
+    for(ii in 1:N){
+      hold_contrast_grp[ , , , , ii, pp] <- outer(hold_grp[ii, pp, , ], hold_grp[ii, pp, , ], '-')
+    }
+    
+    hold_resid[ , , , , , , , , , pp] <- outer(hold_contrast_grp[ , , , , , pp],
+                                               hold_contrasts_oal[ , , , , pp], '-')
   }
   
-  out$outcomes$contrasts <- list(overall = hold_contrasts_oal,
-                                 groups  = hold_contrast_grp,
-                                 group_resid = outer(hold_contrast_grp, 
-                                                hold_contrasts_oal, '-'))
+  out$outcomes$contrasts <- list(overall = drop(hold_contrasts_oal),
+                                 groups  = drop(hold_contrast_grp),
+                                 group_resid = drop(hold_resid))
+  
   ## DONE ####
   return(out)
 }
--- R/ipw_variance_estimates.R
+++ R/ipw_variance_estimates.R
@@ -1,7 +1,13 @@
-#----------------------------------------#
-######       bscore()v              ######
-# compute psi_xp(B_i, X_i, theta)        #
-#----------------------------------------#
+#-----------------------------------------------------------------------------#
+# Compute psi_xp(B_i, X_i, theta)    
+#
+# @param predictors character vector of predictors from model
+# @param B character naming B variable in data
+# @param G character vector (length == 1) naming group variable in data
+# @param theta
+# @param data dataframe
+# @return N X length(theta) matrix of scores
+#-----------------------------------------------------------------------------#
 
 Bscore <- function(predictors, B, G, theta, data){
   N <- length(unique(data[, G]))
@@ -16,134 +22,20 @@ Bscore <- function(predictors, B, G, theta, data){
   return(out)
 }
 
-
-#' Partial U
-#'  
-#' @param predictors
-#' @return length(alpha) vector of IPW estimates
-#' @export
-Upartial <- function(y, G, A, B, data, weights, weight_dervs,
-                     rescale.factor, na.rm = FALSE){
-  out <- list()
-  
-  groups <- as.numeric(dimnames(weight_dervs)[[1]])
-  predictors <- dimnames(weight_dervs[[2]])
-  alphas <- as.numeric(dimnames(weight_dervs)[[3]])
-  trt_lvls <- sort(unique(data[, A]))
-  
-  N <- length(groups)
-  k <- length(alphas)
-  l <- length(trt_lvls)
-  p <- length(theta) - 1
-  
-  # replace any missing weights with 0
-  if(na.rm == FALSE) {
-    weight_dervs[is.na(weight_dervs)] <- 0
-  }
-  
-  ## CALCULATE MARGINAL ESTIMATES ####
-  ybar <- group_means(Y = y, A = A, G = G, a = NA, data = data)
-  grp_est <- apply(weight_dervs, 2:3, function(x) x * ybar) * rescale.factor
-  
-  out$ipw$marginal_outcomes$groups <- grp_est
-  out$ipw$marginal_outcomes$overall <- ymarg <- apply(grp_est, 2, mean, na.rm = TRUE)
-  
-  #   ## CALCULATE OVERALL EFFECTS ####
-  #   out$ipw$effects$oe$overall <- outer(ymarg, ymarg, '-')
-  #   hold_oe_grp <- array(dim = c(k, k, N), dimnames = list(alphas, alphas, groups))
-  #   
-  #   for(ii in 1:N){
-  #     hold_oe_grp[ , , ii] <- outer(grp_est[ii, ], grp_est[ii, ], '-')
-  #   }
-  #   
-  #   out$ipw$effects$oe$groups <- hold_oe_grp
-  
-  ## CALCULATE OUTCOME ESTIMATES PER TREATMENT LEVEL####
-  
-  hold_grp <- hold_grp_diff <- array(dim = c(N, p + 1, k, l),
-                                     dimnames = list(groups, predictors, alphas, trt_lvls))
-  hold_oal <- array(dim = c(p+1, k, l),
-                    dimnames = list(predictors, alphas, trt_lvls))
-  
-  for(ll in 1:l){    
-    a <- trt_lvls[ll]
-    
-    # Compute means per group
-    ybar_trt <- group_means(Y = y, A = A, G = G, a = a, data = data)
-    
-    # Modify weights per treatment level
-    weights_d_trt <- array(dim= c(N, p + 1, k),
-                           dimnames = list(1:N, predictors, alphas))
-    for(pp in 1:(p + 1)){
-      weights_d_trt[ , pp, ] <- t(t(weight_dervs[ , pp, ])/((alphas^a*(1-alphas)^(1-a))))
-    }
-    
-    # Compute estimates
-    grp_est <- apply(weights_d_trt, 2:3, function(x) x * ybar_trt) * rescale.factor
-    oal_est <- apply(grp_est, 2:3, mean, na.rm = TRUE)
-    
-    hold_grp[ , , , ll] <- grp_est
-    hold_oal[ , , ll]   <- oal_est
-    
-    for(alpha in 1:k){
-      for(g in 1:N){
-        hold_grp_diff[g, , alpha, ll] <- hold_grp[g,  , alpha, ll] - hold_oal[ , alpha, ll]
-      }
-    }
-  }
-  
-  out$ipw$outcomes <- list(groups = hold_grp, 
-                           overall = hold_oal, 
-                           group_resid = hold_grp_diff)
-  
-  ## CALCULATE EFFECT CONTRASTS ####
-  
-  hold_contrasts_oal <- array(dim = c(k, l, k, l, p + 1),
-                              dimnames = list(alphas, trt_lvls, alphas, 
-                                              trt_lvls, predictors))
-  
-  hold_contrast_grp <- array(dim = c(k, l, k, l, N, p + 1),
-                             dimnames = list(alphas, trt_lvls, alphas, trt_lvls, 
-                                             groups, predictors))
-  
-  hold_resid <- array(dim = c(k, l, k, l, N, k, l, k, l, p + 1),
-                      dimnames = list(alphas, trt_lvls, alphas, trt_lvls, 
-                                      groups, alphas, trt_lvls, alphas, trt_lvls, 
-                                      predictors))
-  for(pp in 1:(p+1)){
-    hold_contrasts_oal[ , , , , pp] <- outer(hold_oal[pp, , ], hold_oal[pp, , ], '-')
-    
-    for(ii in 1:N){
-      hold_contrast_grp[ , , , , ii, pp] <- outer(hold_grp[ii, pp, , ], hold_grp[ii, pp, , ], '-')
-    }
-    
-    hold_resid[ , , , , , , , , , pp] <- outer(hold_contrast_grp[ , , , , , pp],
-                                               hold_contrasts_oal[ , , , , pp], '-')
-  }
-  
-  
-  out$ipw$contrasts <- list(overall = hold_contrasts_oal,
-                            groups  = hold_contrast_grp,
-                            group_resid = hold_resid)
-  
-  ## DONE ####
-  return(out)
-}
-
-
-
-
+#-----------------------------------------------------------------------------#
 #' V Matrix
 #'  
 #' @param predictors
 #' @return V matrix
 #' @export
+#' 
+#-----------------------------------------------------------------------------#
 V_matrix_effect <- function(Bscores, ipw_obj, alpha1, alpha2, trt.lvl1, trt.lvl2){
 
   N <- ipw_obj$summary$ngroups
-  p <- ipw_obj$summary$npredictors
+  p <- dim(Bscores)[2]
   
-  tmp <- array(dim = c(p+2, p+2, N))
+  tmp <- array(dim = c(p+1, p+1, N))
 
   for(ii in 1:N){
     hold <- c(Bscores[ii, ], ipw_obj$ipw$contrasts$group_resid[alpha1, trt.lvl1, alpha2, trt.lvl2, ii, alpha1, trt.lvl1, alpha2, trt.lvl2])
