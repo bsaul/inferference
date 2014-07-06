e44138595f344833a9b4f919f45ded0d2990f82f
bradley <bradley@bs.local>
2014-07-06 11:18:24 -0400

    -adds calc_effect() which looks up effects and calculates the variance estimate from the various pieces. direct_effect(), indirect_effect(), and total_effect() then just wrap calc_effect() for the appropriate contrasts.

--- R/effect_estimates.R
+++ R/effect_estimates.R
@@ -1,24 +1,28 @@
-#' Calculate IPW direct effect estimates
+#-----------------------------------------------------------------------------#
+#' Calculate Effect estimates
 #'  
 #' @param TBD
 #' @return TBD
 #' @export
+#-----------------------------------------------------------------------------#
 
-
-ipw_de <- function(obj, alpha, trt.lvl1, trt.lvl2){
+calc_effect <- function(obj, alpha1, alpha2, trt.lvl1, trt.lvl2){
   N <- obj[[1]]$summary$ngroups
-  a <- alpha
+  a1 <- alpha1
+  a2 <- alpha2
   t1 <- trt.lvl1
   t2 <- trt.lvl2
-    
-  pe <- obj$point_estimates$outcomes$contrasts$overall[a, t1, a, t2]
+  
+  pe <- obj$point_estimates$outcomes$contrasts$overall[a1, t1, a2, t2]
+  
+  ## VARIANCE ESTIMATION ####
   bscores <- obj$bscores
-
-  V <- V_matrix(bscores, obj[[1]], a, a, t1, t2, 
+  
+  V <- V_matrix(bscores, obj[[1]], a1, a2, t1, t2, 
                 effect = 'contrast', marginal = FALSE )
   
-  U21 <- apply(obj[[3]]$outcomes$contrasts$group_resid[a, t1, a, t2, , a, t1, a, t2, ], 
-                2, mean, na.rm = T)
+  U21 <- apply(obj[[3]]$outcomes$contrasts$group_resid[a1, t1, a2, t2, , a1, t1, a2, t2, ], 
+               2, mean, na.rm = T)
   
   V21 <- V[dim(V)[1], 1:(dim(V)[2] - 1)]
   V11 <- V[1:(dim(V)[1] - 1), 1:(dim(V)[2] - 1)]
@@ -27,10 +31,51 @@ ipw_de <- function(obj, alpha, trt.lvl1, trt.lvl2){
   ve <- ((U21 - 2*V21) %*% solve(V11) %*% U21 + V22)/N
   me <- 1.96 * sqrt(ve)
   
-  out <- paste0('Estimate: ', round(pe, 2), ' 95% CI: (', 
+  toprint <- paste0('Estimate: ', round(pe, 2), ' 95% CI: (', 
                 round(pe - me, 2), ', ', round(pe + me, 2), ')' )
-   
-  return(print(out))
+  
+  print(toprint)
+  
+  out <- c(pe, ve, pe-me, pe+me)
+  return(out)
+}
+
+#-----------------------------------------------------------------------------#
+#' Calculate Direct effect estimates
+#'  
+#' @param TBD
+#' @return TBD
+#' @export
+#-----------------------------------------------------------------------------#
+
+direct_effect <- function(obj, alpha, trt.lvl1 = '0', trt.lvl2 = '1'){
+  out <- calc_effect(obj, alpha, alpha, trt.lvl1, trt.lvl2)
+  return(out)
+}
+
+#-----------------------------------------------------------------------------#
+#' Calculate indirect effect estimates
+#'  
+#' @param TBD
+#' @return TBD
+#' @export
+#-----------------------------------------------------------------------------#
+
+indirect_effect <- function(obj, alpha1, alpha2, trt.lvl = '0'){
+  out <- calc_effect(obj, alpha1, alpha2, trt.lvl, trt.lvl)
+  return(out)
 }
 
 
+#-----------------------------------------------------------------------------#
+#' Calculate indirect effect estimates
+#'  
+#' @param TBD
+#' @return TBD
+#' @export
+#-----------------------------------------------------------------------------#
+
+total_effect <- function(obj, alpha1, alpha2, trt.lvl1 = '0', trt.lvl2 = '1'){
+  out <- calc_effect(obj, alpha1, alpha2, trt.lvl1, trt.lvl2)
+  return(out)
+}
