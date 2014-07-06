a25f7fa8d4e54adfb858bb677e1536031db1977a
bradley <bradley@bs.local>
2014-07-06 11:34:51 -0400

    - splits ipw_variance_estimates.R into two files: V_matrix_calc.R and bscore_calc.R

---
+++ R/bscore_calc.R
@@ -0,0 +1,23 @@
+#-----------------------------------------------------------------------------#
+# Compute psi_xp(B_i, X_i, theta)    
+#
+# @param predictors character vector of predictors from model
+# @param B character naming B variable in data
+# @param G character vector (length == 1) naming group variable in data
+# @param theta
+# @param data dataframe
+# @return N X length(theta) matrix of scores
+#-----------------------------------------------------------------------------#
+
+bscore_calc <- function(predictors, B, G, theta, data){
+  N <- length(unique(data[, G]))
+  
+  out <- matrix(nrow = N, ncol = length(theta))  
+  for(ii in 1:N){
+    grp <- data[data$group == ii, ]
+    X <- as.matrix(cbind(1, grp[, predictors]))
+    out[ii, ] <- cmp_scores_ll(theta = theta, B = grp[ , B], X = X)
+  }
+  
+  return(out)
+}
