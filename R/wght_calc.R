d684b793874b18334be25ba2f8db65c2169e2639
bradley <bradley@bs.local>
2014-07-11 10:34:25 -0400

    - adds silent = TRUE option within wght_deriv_calc to keep grad() errors from being printed.

--- R/wght_calc.R
+++ R/wght_calc.R
@@ -25,12 +25,6 @@ wght_calc <- function(type, A, alpha, ...){
                      alpha = alpha, A = A, ...))
   PrA <- ifelse(is(f, 'try-error'), NA, f$value)
   
-  ## TODO: I tried to write this function so that integrate() could have 
-  # different integrands passed to it from the arguments. It worked for 
-  # computing weights, but was fraught with 'unused argument' errors 
-  # when taking derivatives. I couldn't figure out the problem on first blush
-  # and moved on.
-  
   if (type == 'c'){
     pp <- prod(alpha^A * (1-alpha)^(1-A))
     weight <- pp/PrA
--- R/wght_deriv_calc.R
+++ R/wght_deriv_calc.R
@@ -7,7 +7,8 @@
 wght_deriv_calc <- function(theta, A, X, type, alpha){
   dervs <- sapply(1:length(theta), function(i){
     f <- try(grad(wght_calc, x = theta[i], pos = i, theta = theta, 
-                  type = type, alpha = alpha, A = A, X = X))
+                  type = type, alpha = alpha, A = A, X = X),
+             silent = TRUE)
     return(ifelse(is(f, 'try-error'), NA, f))
   })
   return(dervs)
