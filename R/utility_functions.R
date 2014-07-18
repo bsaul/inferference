29fa2da8878495a02e59995274f0b2775684dcf4
bradley <bradley@maincampusnorth-v884-00199.1xwireless.unc.edu>
2014-07-18 16:35:45 -0400

    -creates get_args(), a utility function to be used for wght_calc()

---
+++ R/utility_functions.R
@@ -0,0 +1,22 @@
+#' Get Arguments
+#' 
+#' Extracts the names of the arguments from a function, and creates a list 
+#' of those arguments where they exist in ... . 
+#' 
+#' @param FUN function for which to find arguments
+#' @param ... any arguments. Those necessary for FUN must be named as appropriate for FUN
+#' @return list of arguments for FUN
+#' @export
+#' @examples
+#' myargs <- get_args(lm, formula = Sepal.Length ~ Sepal.Width, data = iris )
+#' summary(do.call('lm', myargs))
+
+get_args <- function(FUN, ...){
+  dots <- list(...)
+  arg_names <- names(formals(FUN))
+  
+  args <- dots[arg_names]
+  args[sapply(args, is.null)] <- NULL
+  
+  return(args)
+}
