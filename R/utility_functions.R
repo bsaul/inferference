e30e88d7ed3637ea2a69dedaec87385d0cb6c4c3
bradley <bradley@maincampussouth-v883-02989.1xwireless.unc.edu>
2014-08-12 17:27:16 -0400

    - adds group_means to utility_functions.R

--- R/utility_functions.R
+++ R/utility_functions.R
@@ -20,3 +20,32 @@ get_args <- function(FUN, args_list = NULL, ...){
   
   return(args)
 }
+
+#' Calculate outcome means per group per treatment level
+#' 
+#' @param Y name (character vector) of observed outcomes in data
+#' @param A name of treatment variable
+#' @param G name of grouping variable in data
+#' @param a value of treatment level, defaults to NA.
+#' @return data dataset to use
+#' @export
+#'
+
+group_means <- function(Y, A, G, a = NA, data){
+  
+  N <- length(unique(data[ , G]))
+  
+  vals <- by(data, data[ , G], function(x){
+    n <- length(x[ , Y])
+    
+    if(is.na(a)){
+      sum(x[ , Y])/n
+    } else {
+      sum(x[ , Y] * (x[ , A] == a) * 1)/n
+    }
+  })
+  
+  out <- matrix(unlist(vals), nrow = N)
+  
+  return(out)
+}
