d6074de9160d3cea2a864629e7b610483d8a5259
bradley <bradley@bs.local>
2014-08-14 19:13:15 -0400

    -changes to simulation functions. - updates simulation function documentation

--- R/simulation_functions.R
+++ R/simulation_functions.R
@@ -71,7 +71,7 @@ sim_units <- function(N, n,
 
   data <- merge(data.frame(id = 1:n, 
                            group = groups.list$assignments,
-                           X1 = ifelse(X1_ > 100, 100, X1_/10)), 
+                           X1 = ifelse(X1_ > 100, 100/10, X1_/10)), 
                 groups.list$groups, by="group")
   data$X2 <- ind_dist + data$distance_group
   
@@ -79,7 +79,7 @@ sim_units <- function(N, n,
 }
 
 #-----------------------------------------------------------------------------#
-#' TBD
+#' Individual Potential outcomes
 #'
 #' @export
 #-----------------------------------------------------------------------------#
@@ -92,27 +92,8 @@ po_y_ij <- function(dt, a, k, theta){
   return(rbinom(n, 1, p))
 }
 
-# po_y_i <- function(groupdt, theta){
-#   n <- groupdt$group_size[1]
-#   
-#   hold <- sapply(0:1, function(a) {
-#            t(sapply(0:(n-1), function(kk) {
-#              alpha <- (a + kk)/n
-#              X <- cbind(1, a, alpha, groupdt$X1, groupdt$X2, a*alpha)
-#              p <- plogis(X %*% theta)
-#              sapply(p, function(pp) rbinom(1, 1, pp))
-#            }))
-#           }, simplify = 'array')
-#   
-#   dimnames(hold) <- list(id = groupdt$id, k = 0:(n-1), A = 0:1)
-#   
-#   out <- cbind(melt(hold, value.name = 'Y'), n)
-#   
-#   return(out)
-# }
-
 #-----------------------------------------------------------------------------#
-#' TBD
+#' Group level potential outcomes
 #' 
 #'  @export 
 #-----------------------------------------------------------------------------#
@@ -131,47 +112,39 @@ gen_po_ij <- function(dt, theta){
   return(po)
 }
 
-# sim_po <- function(base_dt, theta){
-#   po <- rbind.fill(lapply(split(base_dt, base_dt$group), po_y_i, theta))
-# 
-#   # Merge group information back to potential outcomes
-#   out <- merge(po, base_dt[ , c('id', 'group')], by = 'id')
-#   
-#   return(out)
-# }
-
 #-----------------------------------------------------------------------------#
-#' TBD
+#' Generate Potential Outcomes
 #' @export
 #-----------------------------------------------------------------------------#
 
-make_po <- function(dt, theta){
-  po <- lapply(split(dt, dt$group), gen_po_ij, theta = theta)
+make_po <- function(base, po.parameters){
+  po <- lapply(split(base, base$group), gen_po_ij, theta = po.parameters)
   return(po)
 }
 
-
 #-----------------------------------------------------------------------------#
-#' TBD
+#' Individual level causal estimand Y(a, alpha)
+#' 
+#' Takes n X n - 1 x 2 array as input
+#' @return n x 2 matrix with values of ybar_ij(a, alpha) 
 #' @export
 #-----------------------------------------------------------------------------#
-# --- Takes n X n - 1 x 2 array as input and returns n x 2 matrix with 
-# values of ybar_ij(a, alpha) --- #
 
-ybar_ij_aalpha <- function(dt, alpha){
+
+ybar_ij_aalpha <- function(outcomes, alpha){
   
-  n <- dim(dt)[1]
+  n <- dim(outcomes)[1]
   
   # Keep the IDs along for the ride
-  po_ybar <- array(dim = c(n, 2), dimnames = list(id = dimnames(dt)$id,
-                                                  trt = dimnames(dt)$trt))
+  po_ybar <- array(dim = c(n, 2), dimnames = list(id = dimnames(outcomes)$id,
+                                                  trt = dimnames(outcomes)$trt))
   
   #TODO: Speed up with apply() functions
   for(j in 1:n){
     y0 <- y1 <- 0
     for(k in 0:(n-1)){
-      y0 <- y0 + dt[j, as.character(k), 1] * choose(n - 1, k) * alpha^k*(1-alpha)^(n - k -1)
-      y1 <- y1 + dt[j, as.character(k), 2] * choose(n - 1, k) * alpha^k*(1-alpha)^(n - k -1)
+      y0 <- y0 + outcomes[j, as.character(k), 1] * choose(n - 1, k) * alpha^k*(1-alpha)^(n - k -1)
+      y1 <- y1 + outcomes[j, as.character(k), 2] * choose(n - 1, k) * alpha^k*(1-alpha)^(n - k -1)
     }
     po_ybar[j,1] <- y0
     po_ybar[j,2] <- y1
@@ -180,22 +153,24 @@ ybar_ij_aalpha <- function(dt, alpha){
 }
 
 #-----------------------------------------------------------------------------#
-#' TBD
+#' Individual level marginal causal estimand Y(alpha)
+#' Takes the n x 2 matrix returned by ybar_ij_aalpha
+#' 
+#' @return n x 1 vector of ybar_ij(alpha) 
 #' @export
 #-----------------------------------------------------------------------------#
-
-# --- Takes the n x 2 matrix returned by ybar_ij_aalpha and returns
-# n x 1 vector of ybar_ij(alpha) --- #
-ybar_ij_alpha <- function(dt, alpha){
-  alpha*dt[, 2] + (1 - alpha) * dt[, 1]
+ybar_ij_alpha <- function(input, alpha){
+  alpha*input[, 2] + (1 - alpha) * input[, 1]
 }
 
 #-----------------------------------------------------------------------------#
-#' Estimands
+#' Causal estimands
 #' @export
 #-----------------------------------------------------------------------------#
 
 estimands <- function(potential_outcomes, alpha){
+  
+  # --- Individual average potential outcomes --- #  
   ind_avg_po <- lapply(potential_outcomes, function(x) ybar_ij_aalpha(x, alpha))
   
   # --- Group average potential outcomes --- #                
@@ -203,6 +178,7 @@ estimands <- function(potential_outcomes, alpha){
   grp_avg_po <- matrix(unlist(grp_avg_po), ncol=2, byrow = T)
   # --- Population average potential outcomes --- # 
   pop_avg_po <- apply(grp_avg_po, 2, mean)
+  
   # --- Marginal potential outcomes --- #
   ind_marg_po <- lapply(ind_avg_po, function(x) ybar_ij_alpha(x, alpha))
   grp_marg_po <- unlist(lapply(ind_marg_po, mean))
@@ -222,6 +198,8 @@ calc_estimands <- function(potential_outcomes, alphas){
                          potential_outcomes = potential_outcomes)
   
   nn <- length(alphas)
+  
+  # Create a data.frame of estimands
   truth <- data.frame(alpha = rep(NA, nn), 
                       a0 = rep(NA, nn), 
                       a1 = rep(NA, nn), 
@@ -238,34 +216,33 @@ calc_estimands <- function(potential_outcomes, alphas){
 
 
 #-----------------------------------------------------------------------------#
-#' TBD
+#' Simulate date
 #' @export
 #-----------------------------------------------------------------------------#
 
-sim_data <- function(base_dt, potential_outcomes, theta){
+sim_data <- function(base_dt, potential_outcomes, parameters){
 
   N <- length(unique(base_dt$group))
   n <- nrow(base_dt)
-  theta_fix <- theta[-length(theta)]
+  theta_fix <- parameters[-length(parameters)]
+  rvar <- parameters[length(parameters)]
   ## hood random effect ##
   # neighborhood level random effect from N(0, variance = 1.0859)
   group_effect <- data.frame(group = 1:N, 
-                             b = rnorm(N, mean = 0, sd = sqrt(theta[length(theta)])))
+                             b = rnorm(N, mean = 0, sd = sqrt(rvar)))
   
   dt <- merge(base_dt, group_effect, by='group')
   dt <- within(dt, {
-                    xsum = theta[1] + theta[2]*X1 + theta[3]*X2 + b
-                    B = rbinom(n, 1, p = exp(xsum)/(1 + exp(xsum)))
+                    xsum = theta_fix[1] + theta_fix[2]*X1 + theta_fix[3]*X2 + b
+                    B = rbinom(n, 1, p = plogis(xsum))
                     # Assign Treatments
                     A = ifelse(B == 0, 0, rbinom(n, 1, p=2/3))})
   
-  # Calculate k = # treated in hood i excluding subject j (not sure this needed)
-  # and (a + k)/n_i
+  # Calculate k = # treated in hood i excluding subject j 
   sim_dt <- ddply(dt, .(group), mutate, k = sum(A) - A)
   
-  y <- numeric(n)
-  
   # Observe the outcome
+  y <- numeric(n)
   for(ii in 1:nrow(sim_dt)){
     subj <- sim_dt[ii, ]
     y[ii] <- potential_outcomes[[as.character(subj$group)]][as.character(subj$id), 
@@ -275,8 +252,7 @@ sim_data <- function(base_dt, potential_outcomes, theta){
   
   sim_dt$y <- y
 
-  return(sim_dt[c('y', 'X1', "X2", "A", 'B', 'group')])
-  
+  return(sim_dt[c('y', 'X1', "X2", "A", 'B', 'group')]) 
 }
 
 #-----------------------------------------------------------------------------#
@@ -284,55 +260,33 @@ sim_data <- function(base_dt, potential_outcomes, theta){
 #' @export 
 #-----------------------------------------------------------------------------#
 
-replicate_sims <- function(base_dt, potential_outcomes, nsims, theta){
+replicate_sims <- function(base_dt, 
+                           potential_outcomes, 
+                           nsims,
+                           parameters){
+  
   out <- list(nsims)
   for(sim in 1:nsims){
-    out[[sim]] <- sim_data(base_dt, potential_outcomes, theta)
+    out[[sim]] <- sim_data(base_dt, potential_outcomes, parameters)
   }
   return(out)
 }
 
 #-----------------------------------------------------------------------------#
-#' TBD
+#' Interference Simulations
 #' @export 
 #-----------------------------------------------------------------------------#
 
-# estimands <- function(po, alpha){
-#   
-#   ybar_ij <- function(x, alpha){
-#     sum(x$Y * choose(x$n-1, x$k) * alpha^x$k * (1 - alpha)^(x$n - x$k - 1))
-#   }
-#   
-#   # ybar_ij(a, alpha)
-#   ij.aalpha.hold <- ddply(po, .(group, id, A), ybar_ij, alpha = alpha)
-#   
-#   # ybar_i(a, alpha)
-#   i.aalpha.hold <- ddply(ij.aalpha.hold, .(group, A), summarize,
-#                          ybar_i_aalpha = mean(V1))
-#   
-#   # ybar_i(a, alpha)
-#   aalpha.hold <- ddply(i.aalpha.hold, .(A), summarize,
-#                        ybar_alpha = mean(ybar_i_aalpha))
-#   
-#   ## Marginals
-#   # ybar_ij(alpha)
-#   hold <- merge(ij.aalpha.hold[ij.aalpha.hold$A == 1, c('id', 'group', 'V1')], 
-#                 ij.aalpha.hold[ij.aalpha.hold$A == 0, c('id', 'group', 'V1')], 
-#                 by = c('id', 'group'), suffixes = c('1', '0'))
-#   
-#   hold$ybar_ij_alpha <- alpha*hold$V11 + (1 - alpha) * hold$V10
-#   
-#   # ybar_i(alpha)
-#   i.alpha.hold <- ddply(hold, .(group), summarize, 
-#                         ybar_i_alpha = mean(ybar_ij_alpha))
-#   
-#   # ybar(alpha)
-#   alpha.hold <- mean(i.alpha.hold$ybar_i_alpha)
-#   
-#   out <- data.frame(alpha = alpha, 
-#                     A0 = aalpha.hold[1, 2], 
-#                     A1 = aalpha.hold[2, 2], 
-#                     marg = alpha.hold)
-#   return(out)
-# }
-
+interference_sims <- function(n, N, nsims, base.parameters, parameters, alphas){
+  basedt  <- sim_units(n = n, N = N)
+  this.po <- make_po(basedt, base.parameters)
+  
+  out <- list()
+  out$truth <- calc_estimands(this.po, alphas)  
+  out$sims  <- replicate_sims(base_dt = basedt, 
+                              potential_outcomes = this.po, 
+                              nsims = nsims, 
+                              parameters = parameters)
+  out$base  <- basedt
+  return(out)
+}
