70918cda475b2cb5b4e4052cfda909c27d5bd526
Bradley Saul <bradleysaul@gmail.com>
2014-12-30 18:04:58 -0500

    -adds documentation for datasets

---
+++ R/datasets.R
@@ -0,0 +1,13 @@
+#' Interference Sample Data
+#'
+#' A sample dataset.
+#' 
+#' @format a dataset with 6 variables and 3000 rows
+#' \describe{
+#'    \item{y}{the outcome}
+#'    \item{X1}{a covariate}
+#'    \item{X2}{another covariate}
+#'  }
+#'  @name interference_sample
+#'  
+NULL
