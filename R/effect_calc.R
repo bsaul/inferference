5cb928320814065d434c674699a9cae9961c70ee
Bradley Saul <bradleysaul@gmail.com>
2015-01-08 15:53:27 -0500

    -refactors ipw_effect_calc() to handle variance_estimation options passed from the object created by ipw_interference()

--- R/effect_calc.R
+++ R/effect_calc.R
@@ -51,22 +51,24 @@ ipw_effect_calc <- function(obj,
                             conf.level = 0.95,
                             print = FALSE)
 {
+  
+  allocations <- dimnames(obj$weights)[[2]] 
+  
   ## Warnings ##
   # Print error if either estimates with alpha1 have been computed 
   # or a constrast is being estimated when estimates for alpha2
   # have not been computed
-  if (!(alpha1 %in% obj$summary$allocations) | 
-     (effect_type == 'contrast' & !(alpha2 %in% obj$summary$allocations))){
+  if (!(alpha1 %in% allocations) | (effect_type == 'contrast' & !(alpha2 %in% allocations))){
     stop(paste('At least one of the chosen coverage levels has not been estimated.\n',
                'Select from the following: \n', 
                paste(obj$summary$allocations, collapse = ' ')))
   }
   
   ## Necessary bits ##
-  N  <- obj$summary$ngroups
-  p  <- obj$summary$npredictors
-  k  <- obj$summary$nallocations
-  l  <- obj$summary$ntreatments
+  N  <- dim(obj$weights)[2] 
+  p  <- dim(obj$weightd)[2] 
+  k  <- length(allocations)
+  l  <- dim(obj$point_estimates$outcomes$overall)[2]
   a1 <- as.character(alpha1)
   a2 <- as.character(alpha2)
   t1 <- as.character(trt.lvl1)
@@ -76,49 +78,58 @@ ipw_effect_calc <- function(obj,
   
   oal  <- obj$point_estimates[[fff]]$overall
   grp  <- obj$point_estimates[[fff]]$groups
-  Uoal <- obj$Upart[[fff]]$overall 
-  Ugrp <- obj$Upart[[fff]]$groups
   
-  # Cludgy workaround for case of 1 fixed effect: add dimension to Ugrp array #
-  if(p == 1){
-    names <- dimnames(Ugrp)
-    if(marginal == TRUE){
-      Ugrp <-  array(c(Ugrp[1:N, ], 1, Ugrp[, 1:k]),
-                     dim=c(N, 1, k),
-                     dimnames = list(names[[1]], 'Intercept', names[[2]]))
-    } else {
-      Ugrp <-  array(c(Ugrp[1:N, , ], 1, Ugrp[, 1:k , 1:l]),
-                     dim=c(N, 1, k, l),
-                     dimnames = list(names[[1]], 'Intercept', names[[2]], names[[3]]))
-    }
-
+  if(obj$variance_estimation == 'robust'){
+    Uoal <- obj$Upart[[fff]]$overall 
+    Ugrp <- obj$Upart[[fff]]$groups
     
+    # Cludgy workaround for case of 1 fixed effect: add dimension to Ugrp array #
+    if(p == 1){
+      names <- dimnames(Ugrp)
+      if(marginal == TRUE){
+        Ugrp <-  array(c(Ugrp[1:N, ], 1, Ugrp[, 1:k]),
+                       dim=c(N, 1, k),
+                       dimnames = list(names[[1]], 'Intercept', names[[2]]))
+      } else {
+        Ugrp <-  array(c(Ugrp[1:N, , ], 1, Ugrp[, 1:k , 1:l]),
+                       dim=c(N, 1, k, l),
+                       dimnames = list(names[[1]], 'Intercept', names[[2]], names[[3]]))
+      }
+    }
   }
   
   if(effect_type == 'contrast'){
     if(marginal == TRUE){
       pe          <- oal[a1] - oal[a2]
       pe_grp_diff <- (grp[ , a1] - oal[a1]) - (grp[, a2] - oal[a2])
-      U_pe_grp    <- Ugrp[ , , a1] - Ugrp[ , , a2]  
+      if(obj$variance_estimation == 'robust'){
+        U_pe_grp    <- Ugrp[ , , a1] - Ugrp[ , , a2]
+      }
     } else {
       pe          <- oal[a1, t1] - oal[a2, t2]
       pe_grp_diff <- (grp[ , a1, t1] - oal[a1, t1]) - (grp[ , a2, t2] - oal[a2, t2])
-      U_pe_grp    <- Ugrp[ , , a1, t1] - Ugrp[ , , a2, t2]
+      if(obj$variance_estimation == 'robust'){
+        U_pe_grp    <- Ugrp[ , , a1, t1] - Ugrp[ , , a2, t2]
+      }
     }
   } else {
     if(marginal == TRUE){
       pe          <- oal[a1] 
-      pe_grp_diff <- (grp[ , a1] - oal[a1]) 
-      U_pe_grp    <- Ugrp[ , , a1] 
+      pe_grp_diff <- (grp[ , a1] - oal[a1])
+      if(obj$variance_estimation == 'robust'){
+        U_pe_grp    <- Ugrp[ , , a1]
+      }
     } else {
       pe          <- oal[a1, t1] 
-      pe_grp_diff <- (grp[ , a1, t1] - oal[a1, t1]) 
-      U_pe_grp    <- Ugrp[ , , a1, t1]
+      pe_grp_diff <- (grp[ , a1, t1] - oal[a1, t1])
+      if(obj$variance_estimation == 'robust'){
+        U_pe_grp    <- Ugrp[ , , a1, t1]
+      }
     }
   }
   
   #### VARIANCE ESTIMATION ####
-  if(obj$summary$oracle == FALSE){
+  if(obj$variance_estimation == 'robust'){
     # partial U matrix
     if(p == 1){
       U21 <- sum(-U_pe_grp)/N
@@ -141,7 +152,7 @@ ipw_effect_calc <- function(obj,
     
     ## Sandwich Variance Estimate ##
     ave <- ((U21 - 2*V21) %*% solve(V11) %*% t(U21) + V22)/N * rescale.factor^2
-  } else {
+  } else if(obj$variance_estimation == 'simple'){
     ave <- (1/(N^2)) * (sum((pe_grp_diff)^2, na.rm = T)) * rescale.factor^2
   }
   
