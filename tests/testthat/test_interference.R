32f769e6360349208c6ca65a782ee9633ce8fee6
Bradley Saul <bradleysaul@gmail.com>
2016-07-04 15:34:45 -0400

    modifies test_interference tests to be updated based on current testthat standards. i.e., not using expect_that.

--- tests/testthat/test_interference.R
+++ tests/testthat/test_interference.R
@@ -7,12 +7,12 @@ test_that("Interference() works in various situations", {
   allos  <- c(.35, .4)
   
   # Using GLMER with random effect: should not give error
-  expect_that(interference(data = testdt,
+  expect_error(interference(data = testdt,
                           allocations = allos,
                           propensity_integrand = 'logit_integrand',
                            formula = y | A | B ~ X1 + (1|group) | group,
                            model_method = 'glmer',
-                           method = 'simple'), not(throws_error()))
+                           method = 'simple'), NA)
   
   # Using GLMER with no random effect: should fail
   expect_error(interference(data = testdt,
@@ -31,35 +31,35 @@ test_that("Interference() works in various situations", {
                             method = 'simple'))
   
   # Using GLM: should pass
-  expect_that(interference(data = testdt,
+  expect_error(interference(data = testdt,
                            allocations = allos,
                            propensity_integrand = 'logit_integrand',
                            formula = y | A | B ~ X1 | group,
                            model_method = 'glm',
-                           method = 'simple'), not(throws_error()))
+                           method = 'simple'), NA)
   
   # Using GLM without intercept: should pass
-  expect_that(interference(data = testdt,
+  expect_output(interference(data = testdt,
                            allocations = allos,
                            propensity_integrand = 'logit_integrand',
                            formula = y | A | B ~ 1 | group,
                            model_method = 'glm',
-                           method = 'simple'), not(throws_error()))
+                           method = 'simple'))
   
   # Using Oracle without parameters defined: should fail
-  expect_that(interference(data = testdt,
+  expect_error(interference(data = testdt,
                            allocations = allos,
                            propensity_integrand = 'logit_integrand',
                            formula = y | A | B ~ X1 | group,
                            model_method = 'oracle',
-                           method = 'simple'), throws_error())
+                           method = 'simple'))
 
   # Using Oracle with parameters defined: should pass
-  expect_that(interference(data = testdt,
+  expect_error(interference(data = testdt,
                            allocations = allos,
                            propensity_integrand = 'logit_integrand',
                            formula = y | A | B ~ X1 | group,
                            model_method = 'oracle',
                            model_options = list(fixed.effects = c(1,.5), random.effects = NULL),
-                           method = 'simple'), not(throws_error())) 
+                           method = 'simple'), NA) 
 })
