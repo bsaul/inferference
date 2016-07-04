bdb2d305dd47e5dd0be7a6dddabd9799e0502f63
Bradley Saul <bradleysaul@gmail.com>
2016-07-04 14:46:55 -0400

    updates wght_deriv_array() based on previous modification to wght_calc() functions. Exports this function.

--- R/wght_deriv_array.R
+++ R/wght_deriv_array.R
@@ -1,37 +1,35 @@
 #-----------------------------------------------------------------------------#
-# Create an array of group weight derivatives 
-# 
-# Uses \code{\link{wght_deriv_calc}} to compute the weight derivatives for each 
-# group per coverage level
-#
-# @param integrand the function to passed to the argument 'f' of \code{\link{integrate}},
-# which is part of \code{\link{wght_calc}}. 
-# @param allocations coverage levels in (0, 1), possibly (probably) vector valued
-# @param X covariate matrix
-# @param A vector of treatment assignments
-# @param G vector of group assignments
-# @param fixed.effects vector of fixed effect parameters
-# @param random.effects OPTIONAL vector random effect parameters
-# @param ... additional arguments passed to integrand
-# @return a length(unique(group)) X length(params) X length(alphas) array of 
-# group weight derivatives
-# @export
-# 
+#' Create an array of group weight derivatives
+#' 
+#' Uses \code{\link{wght_deriv_calc}} to compute the weight derivatives for each
+#' group per coverage level
+#' 
+#' @param integrand the function to passed to the argument 'f' of \code{\link{integrate}},
+#' which is part of \code{\link{wght_calc}}.
+#' @param allocations coverage levels in (0, 1), possibly (probably) vector valued
+#' @param X covariate matrix
+#' @param A vector of treatment assignments
+#' @param G vector of group assignments
+#' @param fixed.effects vector of fixed effect parameters
+#' @param random.effects OPTIONAL vector random effect parameters
+#' @param ... additional arguments passed to integrand
+#' @return a length(unique(group)) X length(params) X length(alphas) array of
+#' group weight derivatives
+#' @export
 #-----------------------------------------------------------------------------#
 
-wght_deriv_array <- function(integrand, 
+wght_deriv_array <- function(parameters, 
+                             integrand, 
                              allocations, 
                              X, A, G,
-                             fixed.effects,
-                             random.effects,
-                             runSilent = F, #BB 2015-06-23 #pass in from ipw_interference()
+                             runSilent = FALSE, 
                              ...)
 {
   ## Gather necessary bits ##
   integrand <- match.fun(integrand)
   XX <- cbind(X, A)
-  p  <- length(fixed.effects) # number of predictors
-  pp <- p + length(random.effects)
+  p  <- ncol(X) # number of predictors
+  pp <- length(parameters)
   aa <- sort(allocations) # Make sure alphas are sorted
   gg <- sort(unique(G))
   k  <- length(allocations) 
@@ -41,17 +39,16 @@ wght_deriv_array <- function(integrand,
   ## Warnings ##
 
   ## Compute weight (derivative) for each group, parameter, and alpha level ##
-  if(runSilent != T){print('Calculating array of IP weight derivatives...')} #BB 2015-06-23
+  if(!runSilent) print('Calculating array of IP weight derivatives...')
   
   w.list <- lapply(aa, function(allocation){
     w <- by(XX, INDICES = G, simplify = TRUE, 
             FUN = function(x) {
-              wght_deriv_calc(integrand = integrand, 
+              wght_deriv_calc(parameters = parameters,
+                              integrand  = integrand, 
                               allocation = allocation, 
                               A = x[, p+1], 
                               X = x[, 1:p], 
-                              fixed.effects = fixed.effects, 
-                              random.effects = random.effects,
                               ...)})
     w2 <- matrix(unlist(w, use.names = FALSE), ncol = pp, byrow = TRUE)
     return(w2)}) 
@@ -59,7 +56,7 @@ wght_deriv_array <- function(integrand,
   ## Reshape list into array ##
   out <- array(unlist(w.list, use.names = FALSE), 
                dim = c(N, pp, k),
-               dimnames = list(gg, names(c(fixed.effects, random.effects)), aa))
+               dimnames = list(gg, names(parameters), aa))
   
   return(out)
 }
