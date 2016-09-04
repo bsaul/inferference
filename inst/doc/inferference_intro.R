925a1caf71263c774736b2b02aeda7ab1945455f
Bradley Saul <bradleysaul@gmail.com>
2016-09-03 20:58:46 -0400

    Updates inferference_intro vignette to be the stripped down JSS paper

--- inst/doc/inferference_intro.R
+++ inst/doc/inferference_intro.R
@@ -1,136 +1,150 @@
-## ---- echo = TRUE--------------------------------------------------------
-# load the package
+## ----setup, echo = FALSE, eval = TRUE, cache=FALSE-----------------------
+library(knitr)
+opts_chunk$set(prompt=TRUE)
+options(continue ="+ ")
+
+## ----library, echo = TRUE, eval = TRUE-----------------------------------
 library(inferference)
-
-## ---- echo=TRUE, eval=FALSE----------------------------------------------
-#  alphaz = seq(0, 1, by=.05) # needed to compute truth
-#  theta.base <- c(.5, -0.788, -2.953, -0.098, -0.145, 0.351)
-#  theta.sim <- c(0.2727, -0.0387, .2719, 1.0859)
-#  sample_sim <- sim_interference(n = 3000, N = 250, nsims = 1,
-#                                 base.parameters = theta.base,
-#                                 parameters = theta.sim,
-#                                 alphas = alphaz)
-#  interference_sample <- sample_sim$sims[[1]]
-#  #save(sample_data, file = "data/sample_data.rda")
-
-## ------------------------------------------------------------------------
 head(vaccinesim)
 
-## ---- echo=TRUE----------------------------------------------------------
-sample1 <- interference(
-  allocations = c(.3, .45,  .6), # a minimum of 2 is required
-  formula = y | A | B ~ X1 + X2 + (1|group) | group, 
-  data = vaccinesim, # name of the data frame
-  randomization = 2/3,
-  method = 'simple' # speeds up grad()
-  )
+## ----example1, echo = TRUE, eval = TRUE, results = 'hide', cache = FALSE----
+example1 <- interference(
+    formula = Y | A | B ~ X1 + X2 + (1|group) | group, 
+    allocations = c(.3, .45,  .6), 
+    data = vaccinesim, 
+    randomization = 2/3,
+    method = 'simple')
 
-## ---- echo = TRUE--------------------------------------------------------
-print(sample1)
+## ----example1_summary, echo = TRUE, eval = TRUE--------------------------
+print(example1)
 
-## ---- echo = TRUE--------------------------------------------------------
-# the matrix of weights
-head(sample1$weights)
+## ---- echo = TRUE, eval = TRUE-------------------------------------------
+direct_effect(example1, .3)
+ie(example1, .3)
 
-## ---- echo = TRUE--------------------------------------------------------
-head(sample1$weightd[ , , 1]) # For the 1st allocation 
+## ----example2, echo = TRUE, eval = TRUE, results = 'hide', cache = FALSE----
+example2 <- interference( formula = Y | A | B ~ X1 + X2 + (1|group) | group, 
+    allocations = seq(.2, .8, by = .01), 
+    data = vaccinesim, randomization = 2/3, method = 'simple')
 
-## ---- echo = TRUE--------------------------------------------------------
-head(sample1$scores) 
+## ----deff_plot, echo = TRUE, fig.show = 'hide', fig.width = 6, fig.height = 6----
+deff <- direct_effect(example2)
+x <- deff$alpha1
+y <- as.numeric(deff$estimate)
+u <- as.numeric(deff$conf.high)
+l <- as.numeric(deff$conf.low)
+plot(c(min(x), max(x)), c(-.15, .25), type = 'n', bty = 'l',
+     xlab = expression(alpha), ylab = '' )
+title(ylab = expression(widehat(DE) * "(" * alpha * ")"),
+      line = 2)
+polygon(c(x, rev(x)), c(u, rev(l)), col = 'skyblue', border = NA)
+lines(x, y, cex = 2)
 
-## ---- echo = TRUE--------------------------------------------------------
-sample1$estimates 
+## ----ieff_plot, echo = TRUE, fig.show = 'hide', fig.width = 6, fig.height = 6----
+ieff.4 <- ie(example2, allocation1 = .4)
+x <- ieff.4$alpha2
+y <- as.numeric(ieff.4$estimate)
+u <- as.numeric(ieff.4$conf.high)
+l <- as.numeric(ieff.4$conf.low)
+plot(c(min(x), max(x)),c(-.15, .25), type = 'n', bty = 'l',
+     xlab = expression(alpha * "'"), ylab = '')
+title(ylab = expression(widehat(IE) * "(" * 0.4 * "," * alpha * "'" * ")"),
+      line = 2)
+polygon(c(x, rev(x)), c(u, rev(l)), col = 'skyblue', border = NA)
+lines(x, y, cex = 2)
 
-## ---- echo=TRUE----------------------------------------------------------
-direct_effect(sample1, .3) #DE(.3)/ unfortunately de() is a function in utils.
-ie(sample1, .3, .6) #IE(.3, .6) 
-indirect_effect(sample1, .3, .6) # same
-te(sample1, .3, .6) #TE(.3, .6)
-oe(sample1, .3, .6) #OE(.3, .6)
+## ----ieff_contour, echo = TRUE, fig.show = 'hide', fig.width = 6, fig.height = 6----
+ieff <- subset(example2[["estimates"]], effect == 'indirect')
+x <- sort(unique(ieff$alpha1))
+y <- sort(unique(ieff$alpha2))
+z <- xtabs(estimate ~ alpha1 + alpha2, data= ieff)
+contour(x, y, z, xlab = expression(alpha), 
+        ylab = expression(alpha * "'"), bty = 'l')
 
-# You can use the same functions to get all the effects. This will aid in 
-# plotting (see below.)
+## ----diagnostic, echo = TRUE, eval = TRUE, fig.show = 'hide', fig.width = 4.5, fig.height = 4.5----
+diagnose_weights(example2, allocations = .5, breaks = 30)
 
-# All direct effect of constrast Y(0) - Y(1)
-direct_effect(sample1) 
+## ----voters_data, echo = TRUE, eval = TRUE, cache = FALSE----------------
+voters <-  within(voters, {
+    treated     = (treatment == 1 & reached == 1) * 1 
+    c_age       = (age - mean(age))/10 
+   })
+reach_cnt <- tapply(voters$reached, voters$family, sum)
+voters <- voters[!(voters$family %in% names(reach_cnt[reach_cnt > 1])), ]
+voters <- voters[voters$hsecontact == 1, ]
 
-# to get Y(1) - Y(0) use trt.lvl1 argument
-direct_effect(sample1, trt.lvl1 = 1) 
+## ----voter_coef, echo=TRUE, eval = TRUE, cache = FALSE-------------------
+voters1 <- do.call(rbind, by(voters, voters[, 'family'], function(x) x[1, ]))
+coef.voters <- coef(glm(reached ~ c_age, data = voters1, 
+		               family = binomial(link = 'logit')))
 
-# Other functions work similarly
+## ----propensity_fixed, echo = TRUE, eval = FALSE-------------------------
+#  fixed_propensity <- function(b){
+#  	return(0.5 * dnorm(b))
+#  }
 
-# all indirect effects where treatment = 0 compare to an allocation of .3
-ie(sample1, .3) 
-
-## ---- echo=TRUE----------------------------------------------------------
-sample2 <- interference(
-  allocations = c(.3, .6), # a minimum of 2 is required
-  formula = y | A | B ~ X1 + X2 | group,
-  data = vaccinesim, # name of the data frame
-  model_method = 'glm',
-  model_options = list(family = binomial),
-  randomization = 2/3,
-  method = 'simple' # speeds up grad()
-  )
-print(sample2)
-
-## ---- echo=TRUE----------------------------------------------------------
-sample3 <- interference(
-  allocations = c(.3, .6), # a minimum of 2 is required
-  formula = y | A | B ~ X1 + X2 | group,
-  data = vaccinesim, # name of the data frame
-  model_method = 'oracle',
-  model_options = list(fixed.effects = c(0.2727, -0.0387, .2719),
-                       random.effects = NULL),
-  causal_estimation_options = list(variance_estimation = 'naive'),
-  randomization = 2/3,
-  method = 'simple' # speeds up grad()
-  )
-print(sample3)
-
-## ---- echo=TRUE----------------------------------------------------------
-sample4 <- interference(
-  allocations = c(.3, .6), # a minimum of 2 is required
-  formula = y | A ~ X1 + X2 | group,
-  data = vaccinesim, # name of the data frame
-  model_method = 'glm',
-  causal_estimation_options = list(variance_estimation = 'robust'),
-  method = 'simple' # speeds up grad()
-  )
-print(sample4)
-
-## ---- echo = TRUE--------------------------------------------------------
-myFUN <- function(b, x, pos){
-  return(.5 * dnorm(b))
+## ----household_propensity, echo =TRUE, eval = TRUE-----------------------
+household_propensity <- function(b, X, A, 
+                                 parameters, 
+                                 group.randomization = .5){
+  if(!is.matrix(X)){
+    X <- as.matrix(X)
+  }
+  if(sum(A) == 0){ 
+    pr <-  group.randomization
+  } else { 
+    X.1 <- X[1 ,]; A.1 <- A[1] 
+    h   <- plogis(X.1 %*% parameters)
+    pr  <-  group.randomization * dbinom(A.1, 1, h)
+  }
+  out <- pr * dnorm(b) 
+  out
 }
 
-integrate(myFUN, l = -Inf, u = Inf) # returns 0.5
-
-sample5 <- interference(
-  propensity_integrand = 'myFUN',
-  allocations = c(.3, .6), # a minimum of 2 is required
-  formula = y | A ~ X1 + X2 | group,
-  data = vaccinesim, # name of the data frame
-  model_method = 'glm',
-  causal_estimation_options = list(variance_estimation = 'naive'),
-  # won't work for 'robust' unless addition arguments are defined
-  method = 'simple' # speeds up grad()
-  )
-print(sample5)
-
-## ---- echo = TRUE--------------------------------------------------------
-sample6 <- interference(
-  allocations = seq(.3, .6, by = .01), 
-  formula = y | A ~ X1 + X2 | group,
-  data = vaccinesim, # name of the data frame
-  model_method = 'glm',
+## ----example3, echo=TRUE, eval = TRUE, results = 'hide', cache = FALSE----
+example3 <- interference(
+  formula = voted02p | treated | reached ~ c_age | family,
+  propensity_integrand = 'household_propensity',
+  data = voters,
+  model_method = 'oracle', 
+  model_options = list(fixed.effects = coef.voters, random.effects = NULL),
+  allocations   = c(0, .5),
+  integrate_allocation = FALSE,
   causal_estimation_options = list(variance_estimation = 'robust'),
-  method = 'simple' # speeds up grad()
-  )
+  conf.level = .9)
 
-# Look at the direct effects
-deff <- direct_effect(sample6)
+## ----example3_results, echo=TRUE, eval = TRUE----------------------------
+ie(example3, .5, 0)[ , c('estimate', 'conf.low', 'conf.high')]
 
-# Plot the point estimates
-plot(deff$alpha1, deff$estimate, type = 'l')
+## ----example4, echo= TRUE, eval = TRUE, results = 'hide', cache = FALSE----
+example4 <- interference(
+  formula = voted02p | treated | reached ~ 1 | family,
+  propensity_integrand = 'household_propensity',
+  data = voters,
+  model_method = 'oracle',
+  model_options = list(fixed.effects = 0, random.effects = NULL),
+  allocations   = c(0, .5),
+  integrate_allocation = FALSE,
+  causal_estimation_options = list(variance_estimation = 'naive'),
+  conf.level = .9)
+
+## ----example4_results, echo=TRUE, eval = TRUE----------------------------
+ie(example4, .5, 0)[ , c('estimate', 'conf.low', 'conf.high')]
+
+## ----example4_weights, echo = TRUE---------------------------------------
+G <- tapply(voters[1:12, 'treated'], voters[1:12, 'family'], sum)
+W <- head(example4[["weights"]])[, 2]
+cbind(G, W)
+
+## ----compare_weights, echo = TRUE, eval = TRUE---------------------------
+compare_weights <- function(n, alpha = .5, h = .5){
+  pi  <- rep(alpha, n)
+  PrA <- rep(h, n)
+  c(w1 = prod(pi)/prod(PrA),
+    w2 = 1/prod(PrA/pi),
+    w3 = 1/exp(sum(log(PrA/pi))))  
+}
+
+n <- c(50, 100, 500, 1074, 1075, 10000)
+cbind(n, t(sapply(n, FUN = compare_weights)))
 
